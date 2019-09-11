5a92463cbe664a97775751277d5bbeba0bce3cb4
Isaac Mosquera <imosquer@gmail.com>
2019-09-11 12:00:50 -0700

    adding mock data

--- app/scripts/modules/core/src/builds/Builds.tsx
+++ app/scripts/modules/core/src/builds/Builds.tsx
@@ -1,6 +1,53 @@
 import * as React from 'react';
 
+import { IBuild, IInstanceCounts, IServerGroup } from 'core/domain';
+
+export interface IBuildServerGroups {
+  build: IBuild;
+  serverGroups: IServerGroup[];
+}
 export class Builds extends React.Component {
+  public getApplicationBuilds(): IBuildServerGroups[] {
+    const build: IBuild = {
+      number: 43,
+      result: '0488d5162',
+      building: false,
+      duration: 20,
+      name: 'Some Fake Name',
+      timestamp: new Date(),
+      url: 'http://www.google.com',
+      artifacts: [],
+    };
+
+    const instanceCount: IInstanceCounts = {
+      up: 10,
+      down: 10,
+      starting: 1,
+      succeeded: 1,
+      failed: 1,
+      unkown: 1,
+      outOfService: 1,
+      unknown: 1,
+    };
+
+    const serverGroup: IServerGroup = {
+      account: 'Fake Account',
+      cloudProvider: 'AWS',
+      cluster: 'prod',
+      instanceCounts: instanceCount,
+      instances: [],
+      name: 'prod-myapp-cluster-001',
+      region: 'us-west-1',
+      type: 'aws',
+    };
+
+    const buildSG: IBuildServerGroups = {
+      build: build,
+      serverGroups: [serverGroup],
+    };
+
+    return [buildSG];
+  }
   public render() {
     return (
       <div className="row">
